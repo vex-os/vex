8352d607fddbb238c4079bac65882df232b1b0ab
Kirill GPRB <41549137+undnull@users.noreply.github.com>
2022-10-13 02:01:04 +0500

    Ignore some build temporaries

--- include/sys/version.h
+++ include/sys/version.h
@@ -3,6 +3,6 @@
 #ifndef __INCLUDE_SYS_VERSION_H__
 #define __INCLUDE_SYS_VERSION_H__
 #define VERSION "0.0.0-dev.1"
-#define GIT_REV 0xe81290bb
-#define SEMVER "0.0.0-dev.1+e81290bb"
+#define GIT_REV 0x4fd5b488
+#define SEMVER "0.0.0-dev.1+4fd5b488"
 #endif /* __INCLUDE_SYS_VERSION_H__ */
