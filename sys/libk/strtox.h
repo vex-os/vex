cb789ed9eb0f393c923d18da029df587acc28d3b
Kirill GPRB <41549137+undnull@users.noreply.github.com>
2022-10-15 00:55:53 +0500

    Rename libkern to libk
    Smaller the better

--- sys/libk/strtox.h
+++ sys/libk/strtox.h
@@ -1,11 +1,11 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /* Copyright (c), 2022, Kaneru Contributors */
-#ifndef __SYS_LIBKERN_STRTOX_H__
-#define __SYS_LIBKERN_STRTOX_H__
+#ifndef __SYS_LIBK_STRTOX_H__
+#define __SYS_LIBK_STRTOX_H__
 #include <stdint.h>
 #include <sys/cdefs.h>
 
 const char *_strtox_prelim(const char *restrict s, int *sign, int *base) __weak;
 uintmax_t _strtox_main(const char *restrict *restrict s, int base, uintmax_t error, uintmax_t limval, int limdigit, int *sign) __weak;
 
-#endif /* __SYS_LIBKERN_STRTOX_H__ */
+#endif /* __SYS_LIBK_STRTOX_H__ */
