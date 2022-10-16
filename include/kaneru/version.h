7f9662bd8b7336b491f9c330b6cf1f76324969e4
Kirill GPRB <41549137+undnull@users.noreply.github.com>
2022-10-17 02:05:39 +0500

    i forgor

--- include/kaneru/version.h
+++ include/kaneru/version.h
@@ -3,6 +3,6 @@
 #ifndef __INCLUDE_KANERU_VERSION_H__
 #define __INCLUDE_KANERU_VERSION_H__
 #define K_VERSION "0.0.0-dev.1"
-#define K_GIT_REV 0xd11ecbe9
-#define K_SEMVER "0.0.0-dev.1+d11ecbe9"
+#define K_GIT_REV 0xd8aacdda
+#define K_SEMVER "0.0.0-dev.1+d8aacdda"
 #endif /* __INCLUDE_KANERU_VERSION_H__ */
