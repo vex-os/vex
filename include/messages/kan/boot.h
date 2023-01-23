2b5e55bc2927f23e57570ff6907a1c3cf5f1ec14
omni-works <83828882+omnit3a@users.noreply.github.com>
2023-01-23 10:00:03 -0500

    Add files via upload

---
+++ include/messages/kan/boot.h
@@ -0,0 +1,29 @@
+/* BSD Zero Clause License */
+
+/* Copyright (C) 2022 mintsuki and contributors.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef _MSG_BOOT_H
+#define _MSG_BOOT_H 1
+
+#define UNKNOWN_NAME "unknown"
+#define BOOT_CONSOLE_NAME "boot_console"
+
+#define RECLAIMED_KIB_MSG "boot_info: reclaimed %zu KiB"
+#define BOOT_INFO_VERSION_MSG "boot_info: %s %s"
+#define NO_HHDM_RESPONSE_MSG "boot_info: no hhdm_response"
+#define NO_KERNEL_ADDRESS_RESPONSE_MSG "boot_info: no kernel_address_response"
+#define NO_MEMMAP_RESPONSE_MSG "boot_info: no memmap_response"
+
+#endif
