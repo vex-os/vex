4d017450755e52bcdd3bff5e0562fd9c5cca1362
omni-works <83828882+omnit3a@users.noreply.github.com>
2023-01-29 08:40:22 -0500

    Removed typedef for owner/group id

--- include/kan/vfs.h
+++ include/kan/vfs.h
@@ -7,16 +7,13 @@
 
 #define VFS_FILENAME_LENGTH 255
 
-typedef uint32_t vfs_group_id;
-typedef uint32_t vfs_owner_id;
-
 typedef struct vfs_node_s {
   char name[VFS_FILENAME_LENGTH];
   size_t size;
   size_t nblocks;
   void *direct;
-  vfs_group_id group;
-  vfs_owner_id owner;
+  uint32_t group;
+  uint32_t owner;
   struct vfs_node_s *parent;
   struct vfs_node_s *children; /* NULL for files */
 } vfs_node_t;
