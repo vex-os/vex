997c849bf1335b6598df6dc33de5cb5789c38bb6
omni-works <83828882+omnit3a@users.noreply.github.com>
2023-01-29 08:31:15 -0500

    Changed vfs.h
    Changed:
    - Changed copyright date of license
    - Added owner/group id info

--- include/kan/vfs.h
+++ include/kan/vfs.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
-/* Copyright (c), 2022, KanOS Contributors */
+/* Copyright (c), 2023, KanOS Contributors */
 #ifndef _INCLUDE_KAN_VFS_H__
 #define _INCLUDE_KAN_VFS_H__
 #include <stddef.h>
@@ -7,11 +7,16 @@
 
 #define VFS_FILENAME_LENGTH 255
 
+typedef uint32_t vfs_group_id;
+typedef uint32_t vfs_owner_id;
+
 typedef struct vfs_node_s {
   char name[VFS_FILENAME_LENGTH];
   size_t size;
   size_t nblocks;
   void *direct;
+  vfs_group_id group;
+  vfs_owner_id owner;
   struct vfs_node_s *parent;
   struct vfs_node_s *children; /* NULL for files */
 } vfs_node_t;
