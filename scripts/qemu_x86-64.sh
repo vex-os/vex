115c5a58b719abdd52e45a616ed49b2d33c33dc2
untodesu <41549137+untodesu@users.noreply.github.com>
2024-09-18 15:05:05 +0500

    Update launch script to be able to run under WSL2

--- scripts/qemu_x86-64.sh
+++ scripts/qemu_x86-64.sh
@@ -11,6 +11,18 @@ qargs="${qargs} -vga std"
 qargs="${qargs} -m 512M"
 #qargs="${qargs} -d cpu_reset -d int -no-reboot -no-shutdown"
 
-qemu-system-x86_64 ${qargs}
+if [ -f "/proc/sys/fs/binfmt_misc/WSLInterop" ]
+then
+    # We are running under WSL2; we want to run
+    # a host QEMU distribution and expect its
+    # directory to be present in PATH
+    qemu-system-x86_64.exe ${qargs}
+else
+    # We are running in an actual unix-like
+    # environment; we want to run a system
+    # QEMU installation regardless of whether
+    # it's a host or a guest distribution
+    qemu-system-x86_64 ${qargs}
+fi
 
 printf "\n"
